0f4400912b616ec77578571e260dad3568584344
Saurabh Kushwah <saurabh-kushwah@outlook.com>
2019-10-21 11:28:20 +0530

    Changed DNS Lookup

--- Computer Networks/DNS_Lookup.java
+++ Computer Networks/DNS_Lookup.java
@@ -1,35 +1,37 @@
-
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Scanner ;
+import java.util.Scanner;
+import java.io.IOException;
 
-public class DNS_Lookup {
-	public static void main(String[] args) {
+public class DNS_Lookup{
+
+	public static Scanner inp ;
+	public static InetAddress inet ;
+	public static String url ;
 	
-		Scanner in = new Scanner( System.in );
-		String websiteName ;
-		try {
-			InetAddress inetAddress = InetAddress.getLocalHost();
-			System.out.print(" Enter Website Name : ");
-			websiteName = in.nextLine() ;
-			inetAddress = InetAddress.getByName(websiteName);
-			displayStuff(websiteName, inetAddress);
-			System.out.println("--------------------------");
-			InetAddress[] inetAddressArray = InetAddress.getAllByName(websiteName);
-			for (int i = 0; i < inetAddressArray.length; i++) {
-				displayStuff( websiteName +" #" + (i + 1), inetAddressArray[i]);
+	public static void main(String args[]){
+		try{
+			System.out.print("Enter Website Name : ");
+			inp = new Scanner(System.in);
+			url = inp.nextLine();
+			inet = InetAddress.getByName( url );
+
+			System.out.println( "Website Information : " + inet);
+			InetAddress arr[] = InetAddress.getAllByName(url);
+			for( InetAddress i : arr ){
+				display(i);
 			}
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+		}catch(IOException e){
+			System.out.println(e.getMessage());
 		}
+		
+
 	}
 
-	public static void displayStuff(String whichHost, InetAddress inetAddress) {
-		System.out.println("--------------------------");
-		System.out.println("Which Host:" + whichHost);
-		System.out.println("Canonical Host Name:" + inetAddress.getCanonicalHostName());
-		System.out.println("Host Name:" + inetAddress.getHostName());
-		System.out.println("Host Address:" + inetAddress.getHostAddress());
+	public static void display(InetAddress inet ){
+		System.out.println(" ---------------------");
+		System.out.println( "Canonical Host Name : " + inet.getCanonicalHostName() );
+		System.out.println( "Host Name : " + inet.getHostName() );
+		System.out.println( "Host Address : " + inet.getHostAddress() );
 	}
+
 }
-
